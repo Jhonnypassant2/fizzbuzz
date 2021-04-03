fa4f746b168fd6937707ef3ad87bae906a5dc7ec
Jhonnypassant2 <69731773+Jhonnypassant2@users.noreply.github.com>
2021-04-03 08:13:08 -0500

    Initial commit

---
+++ fizz.js
@@ -0,0 +1,36 @@
+fizzBuzz(prompt("ingrese un numero"));
+
+function fizzBuzz(max)
+{
+    for(var i = 1; i <= max; i++)
+    {
+        if(esDivisible(i, 3))
+        {
+        document.write("Fizz");
+        }
+
+        if(esDivisible(i, 5))
+        {
+            document.write("Buzz");
+        }
+
+        if(!esDivisible(i, 3) && !esDivisible(i, 5))
+        {
+            document.write(i);
+        }
+        document.write("<br />");
+    }
+
+    function esDivisible(num, divisor)
+    {
+        if(num % divisor == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
